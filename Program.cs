0c835098d736ce7fb2adbdc8e5993734588faf73
CallePalsson <carlerikuno@gmail.com>
2025-09-26 14:31:24 +0200

    Added: constructor , Menu , Logout
    added a constructor in item.cs, added a user menu for logged in users, added a logout button for logged in users

--- Program.cs
+++ Program.cs
@@ -1,10 +1,11 @@
 ﻿using System.ComponentModel.Design;
+using System.Runtime.CompilerServices;
 using System.Transactions;
 using App;
 
-// A user needs to be able to register an account
-// A user needs to be able to log out.
-// A user needs to be able to log in.
+// A user needs to be able to register an account !!
+// A user needs to be able to log out.!!
+// A user needs to be able to log in.!!
 // A user needs to be able to upload information about the item they wish to trade.
 // A user needs to be able to browse a list of other users items.
 // A user needs to be able to request a trade for other users items.
@@ -13,6 +14,7 @@ using App;
 // A user needs to be able to deny a trade request.
 // A user needs to be able to browse completed requests.
 List<User> users = new List<User>();
+List<Item> tradecenter = new List<Item>();
 
 
 users.Add(new User("Calle" , "123"));
@@ -52,10 +54,18 @@ while (running)
     }
     while (ActiveUser)
     {
+
         Console.WriteLine("Logged in as: ");
+        Console.WriteLine("--------------------Tradecenter------------------");
+        Console.WriteLine("1. Logout");
+        Console.WriteLine("2. Add a item to tradecenter");
+        Console.WriteLine("3. ");
         switch (Console.ReadLine())
         {
             case "1":
+                ActiveUser = false;
+                Console.WriteLine("Logging out!");
+                Console.ReadLine();
                 break;
             case "2":
                 break;
