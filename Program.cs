fd4184a8864615ae9f3a1b592faa1923e4c6f99b
CallePalsson <carlerikuno@gmail.com>
2025-09-27 13:32:43 +0200

    Added some comments
    Added comments on the things that are done atm

--- Program.cs
+++ Program.cs
@@ -6,48 +6,54 @@ using App;
 // A user needs to be able to register an account !!
 // A user needs to be able to log out.!!
 // A user needs to be able to log in.!!
-
-// A user needs to be able to upload information about the item they wish to trade.
+// A user needs to be able to upload information about the item they wish to trade. !!
 
 // A user needs to be able to browse a list of other users items.
+
 // A user needs to be able to request a trade for other users items.
 // A user needs to be able to browse trade requests.
 // A user needs to be able to accept a trade request.
 // A user needs to be able to deny a trade request.
 // A user needs to be able to browse completed requests.
+
+//List of users and items to store users inputs
 List<User> users = new List<User>();
 List<Item> items = new List<Item>();
+
+//Dictionary to store a complete advertisment
 Dictionary<Item, User> tradecenter = new Dictionary<Item, User>(); 
 
 users.Add(new User("a" , "a"));
-
 bool running = true;
 
 while (running)
 {
-    foreach (var user in users)
-    {
-        Console.WriteLine(user.Username);
-    }
-    bool ActiveUser = false;
+    bool ActiveUser = false; //ActiveUser false becuase no one is logged in
+
+    //Menu for login options
     Console.WriteLine($"Welcome to tradecenter!\n\n");
     Console.WriteLine("1. Login");
     Console.WriteLine("2. Create a account");
     switch (Console.ReadLine())
     {
+        //Asking the user for login information 
         case "1":
             Console.Write("Enter username: ");
             string? L_username = Console.ReadLine();
             Console.Write("Enter password: ");
             string? L_password = Console.ReadLine();
+            
+            //Looping through users list and checking if there is a user with the users inputs
             foreach (var user in users)
             {
+
                 if (user.Trylogin(L_username, L_password) == true)
                 {
                     ActiveUser = true;
-                    string CurrentUser = user.Username;
                 }
+                string CurrentUser = user.Username;
             }
+
             break;
         case "2":
             Console.Write("Enter username: ");
@@ -64,7 +70,7 @@ while (running)
         Console.WriteLine("--------------------Tradecenter------------------");
         Console.WriteLine("1. Logout");
         Console.WriteLine("2. Add a item to tradecenter");
-        Console.WriteLine("3. ");
+        Console.WriteLine("3. Browse the tradecenter");
         switch (Console.ReadLine())
         {
             case "1":
@@ -78,11 +84,13 @@ while (running)
                 Console.Write("A short description of that item: ");
                 string? description = Console.ReadLine();
                 items.Add(new Item(item, description));
+                
+                
                 // foreach (var i in items)
                 // {
                 //     Console.WriteLine(i.Name);
                 //     Console.WriteLine($"Description\n{i.Description}");
-                    
+
                 // }
                 break;
         }
